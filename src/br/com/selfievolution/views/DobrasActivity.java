198671d76d7da661007bae130dabb7b89cd80bd7
danielvazdesouza <daniel.vaz35@gmail.com>
2014-09-22 21:28:45 -0300

    Dobras
    Adicionado activity dobras e alterado icone de app

---
+++ src/br/com/selfievolution/views/DobrasActivity.java
@@ -0,0 +1,13 @@
+package br.com.selfievolution.views;
+
+import br.com.selfievolution.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DobrasActivity extends Activity{
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dobras);
+	}
+}
